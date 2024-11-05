8eee00b54dc4f5909edf103ddf9760914f3a0cfe
Jason Hunter <Jason.Hunter@computastar.com>
2024-11-05 15:03:28 +0000

    Added Bool

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            double pi = 3.14;
+
+            Console.WriteLine(pi);
+
+            bool learningCSharp = true;
+            
+            Console.WriteLine(learningCSharp);
         }
     }
 }
